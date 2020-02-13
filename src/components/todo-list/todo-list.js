7644537ee4d64e13dfd109245620573eafe187aa
Sarah Richards <sarah.richards@amazerealise.com>
2020-02-13 20:39:56 +0000

    Accessibility tweaks and mobile

--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -6,12 +6,16 @@ import { Task } from '../task/task.js';
 let template =/*html*/`
 		<section class="container">
 			<form class="todo">
-				<h1 class="todo__header h3">
-					{{ this.header }}
-				</h1>
-				<ul class="todo__list">
-					<Task v-for="task in this.tasks" v-bind:key="task.id" :title="task.title" :id="task.id" :importance="task.importance" :isDoneInitial="task.isDone"/>
-				</ul>
+				<header>
+					<h1 class="todo__header h3">
+						{{ this.header }}
+					</h1>
+				</header>
+				<section>
+					<ul class="todo__list">
+						<Task v-for="task in this.tasks" v-bind:key="task.id" :title="task.title" :id="task.id" :importance="task.importance" :isDoneInitial="task.isDone"/>
+					</ul>
+				</section>
 			</form>
 		</section>
 `;
