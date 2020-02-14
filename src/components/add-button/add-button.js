c533e440e34595d687134ad1cac9d1bfdb7bed5c
Sarah Richards <sarah.richards@amazerealise.com>
2020-02-14 19:40:44 +0000

    Skeleton add button component

---
+++ src/components/add-button/add-button.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+let template =
+/*html*/`<button>Add item</button>
+		 <div class="add-button__overlay">
+		 </div>
+`;
+
+export let Task = {
+	data: function() {
+		return {
+			
+		}
+	},
+	computed: {
+		
+	},
+	methods: {
+		
+	},
+	template: template,
+};
